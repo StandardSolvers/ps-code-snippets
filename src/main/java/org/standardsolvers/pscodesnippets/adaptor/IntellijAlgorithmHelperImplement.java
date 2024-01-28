40e71467239e2d64f973412f01bd26eb8623262e
lshh <markhong93@naver.com>
2024-01-28 21:57:22 +0900

    init file structure + 명칭 오타 수정

--- src/main/java/org/standardsolvers/pscodesnippets/adaptor/IntellijAlgorithmHelperImplement.java
+++ src/main/java/org/standardsolvers/pscodesnippets/adaptor/IntellijAlgorithmHelperImplement.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 // 인텔리제이 플러그인 연결에 대한 구체 정보를 구현합니다.
 // 알고리즘 제공에 대한 구체 정보를 구현합니다.
-public class IntellijAlgorithmAdaptorImplement implements IntellijAlgorithmHelper {
+public class IntellijAlgorithmHelperImplement implements IntellijAlgorithmHelper {
     @Override
     public List<String> findList() {
         return null;
