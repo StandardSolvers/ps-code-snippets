8701a5a54e254273085785b9e5c8f2ed17f0ed21
lshh <markhong93@naver.com>
2024-01-28 22:03:55 +0900

    init file structure + Algorithm 불필요 항목 삭제 및 수정

--- src/main/java/org/standardsolvers/pscodesnippets/solution/Algorithm.java
+++ src/main/java/org/standardsolvers/pscodesnippets/solution/Algorithm.java
@@ -1,14 +1,10 @@
 package org.standardsolvers.pscodesnippets.solution;
 
 public interface Algorithm {
-    // 설정 인자
-    <T> Algorithm setConfig(String string, T argument);
 
     // 컨텍스트 인자
-    <T> Algorithm setProperty(String string, T argument);
+    <T> Algorithm setProperty(String key, T value);
 
     // 알고리즘 바디
     String getContext();
-
-
 }
