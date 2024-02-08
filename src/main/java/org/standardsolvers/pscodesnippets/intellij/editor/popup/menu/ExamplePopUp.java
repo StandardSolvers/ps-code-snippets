e67696ca6fdf74682ba5f040bdfef6ffb7b73578
connieya <gunny6026@naver.com>
2024-02-08 10:59:17 +0900

    feat : 선택한 ps 코드 현재 커서에 삽입

---
+++ src/main/java/org/standardsolvers/pscodesnippets/intellij/editor/popup/menu/ExamplePopUp.java
@@ -0,0 +1,13 @@
+package org.standardsolvers.pscodesnippets.intellij.editor.popup.menu;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.psi.PsiManager;
+
+public class ExamplePopUp extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+
+    }
+}
--- src/main/java/org/standardsolvers/pscodesnippets/intellij/editor/popup/menu/PsPopUp.java
+++ src/main/java/org/standardsolvers/pscodesnippets/intellij/editor/popup/menu/PsPopUp.java
@@ -2,6 +2,15 @@ package org.standardsolvers.pscodesnippets.intellij.editor.popup.menu;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.EditorFactory;
+import com.intellij.openapi.editor.colors.EditorColors;
+import com.intellij.openapi.editor.colors.EditorColorsManager;
+import com.intellij.openapi.editor.colors.EditorFontType;
+import com.intellij.openapi.editor.markup.*;
+import com.intellij.openapi.project.Project;
 import org.standardsolvers.pscodesnippets.intellij.dialog.wrapper.PsDialogWrapper;
 
 public class PsPopUp extends AnAction {
@@ -11,5 +20,12 @@ public class PsPopUp extends AnAction {
         // TODO: insert action logic here
         PsDialogWrapper dialog = new PsDialogWrapper();
         dialog.show();
+        String context = dialog.getContext();
+        Project project = e.getProject();
+        Editor editor = (Editor) e.getDataContext().getData("editor");
+        int offset = editor.getCaretModel().getOffset();
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            editor.getDocument().insertString(offset, context);
+        });
     }
 }
